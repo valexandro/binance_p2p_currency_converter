09fdef78ebdd8673654adea2d95e840b0db64a17
Slava Aleksandrov <shark1501@gmail.com>
2022-09-20 22:59:31 +1000

    grammar fix

--- converter_project/core/templatetags/user_filters.py
+++ converter_project/core/templatetags/user_filters.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 @register.filter
 def divide(value, arg):
-    """Divide value bu arg."""
+    """Divide value by arg."""
     try:
         return round(float(value) / float(arg), 3)
     except (ValueError, ZeroDivisionError):
