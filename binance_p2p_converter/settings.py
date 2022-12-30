8f608d97a4d966169a9151c5a4ea43b38f74cb50
valexandro <shark1501@gmail.com>
2022-12-30 18:28:22 +1000

    Hide secret key

--- binance_p2p_converter/settings.py
+++ binance_p2p_converter/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 import sys
 from pathlib import Path
+from dotenv import load_dotenv
 
 from django.contrib.messages import constants as messages
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oqkbh#7^fy76lxl-a&^esej!zz!@uul0s%g3%0_du_1m_j#oi)'
+SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
